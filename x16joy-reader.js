fc18bf30673d40e9e85ab8506a2cae3819094c90
vlakoosh <vlakoosh@gmail.com>
2024-05-11 23:46:35 +0200

    refactoring and added a test which is currently not working :(
    Signed-off-by: Szymon Walkusz <szymon.walkusz@student.kdg.be>

---
+++ x16joy-reader.js
@@ -0,0 +1,78 @@
+function doOperation(operation, values, currentValue){
+    switch (operation){
+        case "multiply":
+            values[currentValue] = values[currentValue] * values[currentValue - 1];
+            values[currentValue - 1] = 0;
+            break;
+        case "add":
+            values[currentValue] = values[currentValue] + values[currentValue - 1];
+            values[currentValue - 1] = 0;
+            break;
+        case "subtract":
+            values[currentValue] = values[currentValue] - values[currentValue - 1];
+            values[currentValue - 1] = 0;
+            break;
+    }
+}
+
+function calculateCodeSums(code){
+    let instructions = code.split("\n");
+    let instructionSums = [];
+    for (let i = 0; i < instructions.length; i++){
+        instructionSums.push(0);
+        instructionSums[i] = calculateCommandSum(instructions[i]);
+    }
+    return instructionSums
+}
+
+function calculateCommandSum(codeLine){
+    let sum = 0;
+    let values = [0];
+    let currentValue = 0;
+    let currentOperation = "";
+    let space = 0;
+
+    for (let i = 0; i < codeLine.length; i++) {
+        if (codeLine.at(i) + codeLine.at(i+1) === "😂" && space === 0){
+            values[currentValue]++;
+        }
+        else if (codeLine.at(i) + codeLine.at(i+1) === "😂" && space === 1){
+            currentOperation = "multiply";
+            values.push(0);
+            currentValue++
+            values[currentValue]++;
+            space = 0;
+        }
+        else if (codeLine.at(i) + codeLine.at(i+1) === "😂" && space === 3){
+            currentOperation = "add";
+            values.push(0);
+            values.push(0);
+            currentValue+=2;
+            values[currentValue]++;
+            space = 0;
+        }
+        else if (codeLine.at(i) === " " && space === 0){
+            doOperation(currentOperation, values, currentValue);
+            space++;
+        }
+        else if (codeLine.at(i) === " " && space > 0){
+            space++;
+            if (space === 3){
+                currentOperation = "add";
+            }
+        }
+    }
+
+    doOperation();
+
+    values.forEach(value => {
+        sum += value;
+    })
+
+    return sum;
+}
+
+let code = "instruction😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂   😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂   😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂   😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂   😂😂😂😂 😂😂😂😂 😂😂😂😂   😂😂😂😂 😂😂😂😂 😂😂   😂😂😂😂 😂😂😂😂   😂😂😂😂";
+
+
+console.log(calculateCommandSum(code));
