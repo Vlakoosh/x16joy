5ddbf01ef8c13b6a8d15ec8b6a22a9ac4d5cd550
vlakoosh <vlakoosh@gmail.com>
2024-05-13 13:34:12 +0200

    updated writer test added basic html display and code reader
    Signed-off-by: Szymon Walkusz <szymon.walkusz@student.kdg.be>

--- tests/x16joy-writer.test.js
+++ tests/x16joy-writer.test.js
@@ -1,6 +1,7 @@
 let result1 = "😂😂 😂😂 😂😂 😂😂 😂😂 😂😂 😂😂 😂😂 😂😂 😂😂 😂😂 😂😂";
 let result2 = "😂";
 let result3 = "😂😂 😂😂 😂😂 😂😂   😂😂 😂😂 😂😂";
+let result4 = "😐instruction😐😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂   😐value😐😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂   😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂   😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂😂😂 😂😂   😂😂😂😂 😂😂😂😂 😂😂😂😂   😂😂😂😂 😂😂😂😂 😂😂   😂😂😂😂 😂😂😂😂   😐register number😐😂😂😂😂";
 
 const x16joy_writer = require("../x16joy-writer.js");
 const x16joy_reader = require("../x16joy-reader.js");
@@ -15,4 +16,8 @@ test('should return a correct joyCode value', () => {
 
 test('should return a correct joyCode value', () => {
     expect(x16joy_reader.calculateCommandSum(x16joy_writer.convertNumberToJoyCode(24))).toBe(x16joy_reader.calculateCommandSum(result3));
+});
+
+test('should return a correct joyCode value', () => {
+    expect(x16joy_reader.calculateCommandSum(x16joy_writer.convertNumberToJoyCode(7796))).toBe(x16joy_reader.calculateCommandSum(result4));
 });
