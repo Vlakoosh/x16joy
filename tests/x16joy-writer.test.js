0e68f937564dacfd664a02c5e3ba2d9d8b49d63c
vlakoosh <vlakoosh@gmail.com>
2024-05-13 13:04:02 +0200

    made the x16joy-writer.test.js more forgiving to check the value of string instead of comparing the string directly as there can be multiple different ways of representing a joy-code value
    Signed-off-by: Szymon Walkusz <szymon.walkusz@student.kdg.be>

--- tests/x16joy-writer.test.js
+++ tests/x16joy-writer.test.js
@@ -3,15 +3,16 @@ let result2 = "😂";
 let result3 = "😂😂 😂😂 😂😂 😂😂   😂😂 😂😂 😂😂";
 
 const x16joy_writer = require("../x16joy-writer.js");
+const x16joy_reader = require("../x16joy-reader.js");
 
 test('should return a correct joyCode value', () => {
-    expect(x16joy_writer.convertNumberToJoyCode(4096)).toBe(result1);
+    expect(x16joy_reader.calculateCommandSum(x16joy_writer.convertNumberToJoyCode(4096))).toBe(x16joy_reader.calculateCommandSum(result1));
 });
 
 test('should return a correct joyCode value', () => {
-    expect(x16joy_writer.convertNumberToJoyCode(1)).toBe(result2);
+    expect(x16joy_reader.calculateCommandSum(x16joy_writer.convertNumberToJoyCode(1))).toBe(x16joy_reader.calculateCommandSum(result2));
 });
 
 test('should return a correct joyCode value', () => {
-    expect(x16joy_writer.convertNumberToJoyCode(24)).toBe(result3);
+    expect(x16joy_reader.calculateCommandSum(x16joy_writer.convertNumberToJoyCode(24))).toBe(x16joy_reader.calculateCommandSum(result3));
 });
